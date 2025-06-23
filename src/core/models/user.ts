9592d88aa76fcf69f71cd9075fa0bbdf56f4226d
ahmeddavids <ahmeddavids6@gmail.com>
2025-06-23 20:36:37 +0100

    fix(user): ensure email is stored in lowercase in the User schema

--- src/core/models/user.ts
+++ src/core/models/user.ts
@@ -7,7 +7,7 @@ const UserSchema: Schema = new Schema(
   {
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true },
     phone: { type: String, required: true },
     gender: { type: String, required: true },
