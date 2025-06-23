27d101318cee887f8f506ca5d5126ddfa06828eb
ahmeddavids <ahmeddavids6@gmail.com>
2025-06-23 20:29:13 +0100

    feat(types): add Multer file types to Express Request interface

---
+++ src/types/express/index.d.ts
@@ -0,0 +1,9 @@
+import { Request } from 'express';
+import { File as MulterFile } from 'multer';
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    file?: MulterFile; // for req.file from .single()
+    files?: MulterFile[] | { [fieldname: string]: MulterFile[] }; // for .array() or .fields()
+  }
+}
