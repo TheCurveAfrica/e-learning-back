744043a1f6428ba90a226fc9ffc5a269f1eff4eb
ahmeddavids <ahmeddavids6@gmail.com>
2025-09-05 13:30:25 +0100

    feat(routes): add admin route to base route for admin authentication

--- src/api/routes/Base.ts
+++ src/api/routes/Base.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRoute from './user';
+import adminRoute from './admin';
 
 const baseRoute = Router();
 
 baseRoute.use('/auth/users', authRoute);
+baseRoute.use('/auth/admins', adminRoute);
 
 export default baseRoute;
