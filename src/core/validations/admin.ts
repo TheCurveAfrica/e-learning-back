f44a85b5fe2a995b3e8c4302de2eb48f28bc3f03
ahmeddavids <ahmeddavids6@gmail.com>
2025-09-06 16:26:07 +0100

    feat(admin) Updated the validation enum to ensure maintainability and keeps validation in sync with the enum definitions.

--- src/core/validations/admin.ts
+++ src/core/validations/admin.ts
@@ -7,10 +7,7 @@ export const adminRegistrationSchema = z.object({
   lastname: z.string().trim().min(1, 'Lastname is required').max(50, 'Lastname must be at most 50 characters long'),
   email: z.string().email().toLowerCase().trim().min(1, 'Email is required').max(100, 'Email must be at most 100 characters long'),
   phone: phoneValidator.optional().or(z.literal(' ')),
-  role: z
-    .enum([USER_ROLES.ADMIN, USER_ROLES.INSTRUCTOR])
-    .transform((val) => val.toLowerCase())
-    .default(USER_ROLES.INSTRUCTOR),
+  role: z.nativeEnum(USER_ROLES).default(USER_ROLES.INSTRUCTOR),
   profilePicture: z.string().optional(),
   password: passwordValidator.optional()
 });
