dc4c3c6777cdc1cf69ca91e83d6c5d31ca1daff4
Anthony0519 <anthonyodoh16@gmail.com>
2025-10-01 05:47:44 +0100

    feat: Implemented Admin Dashboard

--- src/api/routes/Base.ts
+++ src/api/routes/Base.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import authRoute from './user';
 import adminRoute from './admin';
 import classRoute from './class';
+import dashboardRoute from './dashboard';
 
 const baseRoute = Router();
 
 baseRoute.use('/auth/users', authRoute);
 baseRoute.use('/auth/admins', adminRoute);
 baseRoute.use('/classes', classRoute);
+baseRoute.use('/dashboard', dashboardRoute);
 
 export default baseRoute;
